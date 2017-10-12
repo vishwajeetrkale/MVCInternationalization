b7b36c41ce83934b496f505312976e7207630a78
vishwajeet <vishwajeetrkale@gmail.com>
2017-10-12 19:38:23 +0530

    OFFC

---
+++ MvcInternationalization/MvcInternationalization/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using MvcInternationalization.Helper;
+using MvcInternationalization.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcInternationalization.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Person per)
+        {
+            return View();
+        }
+        public ActionResult SetCulture(string culture)
+        {
+            // Validate input
+            culture = CultureHelper.GetImplementedCulture(culture);
+            // Save culture in a cookie
+            HttpCookie cookie = Request.Cookies["_culture"];
+            if (cookie != null)
+                cookie.Value = culture;   // update cookie value
+            else
+            {
+                cookie = new HttpCookie("_culture");
+                cookie.Value = culture;
+                cookie.Expires = DateTime.Now.AddYears(1);
+            }
+            Response.Cookies.Add(cookie);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
